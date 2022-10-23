cd54df286cc50054d3adb6c2e7f1618d7b58b6bc
Klaudiusz Sołtysik <klaudiusz.s1405@gmail.com>
2022-10-23 15:54:54 +0200

    23.10 work

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
 
+url = ("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4e0ad1a0acb34407a06c85ae68a19813")
 
-def main(request):
-    return render(request=request, template_name='main.html')
+def home(request):
+    response = requests.get(url).json()['articles']
+
+    return render(request, 'home.html', {'response': response[0:5]})
